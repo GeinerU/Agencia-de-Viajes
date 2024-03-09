3449509cc9dcdae70bbbcbb8316af040b717e392
geiner <uribegeiner03@gmail.com>
2024-03-09 02:15:39 -0500

    Se agrega codigo correspondiente a HotelesOfrecidos.js

--- proyecto-agencia/proyecto-agencia/src/components/HotelesOfrecidos/HotelesOfrecidos.js
+++ proyecto-agencia/proyecto-agencia/src/components/HotelesOfrecidos/HotelesOfrecidos.js
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+import './HotelesOfrecidos.css';
+
+function HotelesOfrecidos() {
+    return (
+        <section id="hoteles">
+            {/* Contenido de la sección Hoteles Ofrecidos */}
+        </section>
+    );
+}
+
+export default HotelesOfrecidos;
