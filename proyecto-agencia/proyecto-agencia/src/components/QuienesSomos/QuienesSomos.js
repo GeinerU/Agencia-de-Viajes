1b75102d5e6405d901bbf40207d8707f0b88a573
geiner <uribegeiner03@gmail.com>
2024-03-09 02:18:41 -0500

    Se agrega codigo correspondiente a QuienesSomos.js

--- proyecto-agencia/proyecto-agencia/src/components/QuienesSomos/QuienesSomos.js
+++ proyecto-agencia/proyecto-agencia/src/components/QuienesSomos/QuienesSomos.js
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+import './QuienesSomos.css';
+
+function QuienesSomos() {
+    return (
+        <section id="quienesSomos">
+            {/* Contenido de la sección Quienes Somos */}
+        </section>
+    );
+}
+
+export default QuienesSomos;
