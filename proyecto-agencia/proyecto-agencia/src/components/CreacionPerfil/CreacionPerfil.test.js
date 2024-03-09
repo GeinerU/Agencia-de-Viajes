e89c0031114cf58c0904b51d89cd01c7d7c4cfe3
geiner <uribegeiner03@gmail.com>
2024-03-09 02:45:12 -0500

    Se agrega codigo para prueba de Destinos y Precios en archivo en DestinosPrecios.test.js

--- proyecto-agencia/proyecto-agencia/src/components/CreacionPerfil/CreacionPerfil.test.js
+++ proyecto-agencia/proyecto-agencia/src/components/CreacionPerfil/CreacionPerfil.test.js
@@ -1,1 +1,9 @@
- 
+// Importa las bibliotecas necesarias para las pruebas
+import React from 'react';
+import { render } from '@testing-library/react';
+import CreacionPerfil from './CreacionPerfil';
+
+// Prueba básica para verificar que el formulario de creación de perfil se renderiza correctamente
+test('Renderiza el formulario de creación de perfil correctamente', () => {
+    render(<CreacionPerfil />);
+});
--- proyecto-agencia/proyecto-agencia/src/components/DestinosPrecios/DestinosPrecios.test.js
+++ proyecto-agencia/proyecto-agencia/src/components/DestinosPrecios/DestinosPrecios.test.js
@@ -1,1 +1,9 @@
- 
+// Importa las bibliotecas necesarias para las pruebas
+import React from 'react';
+import { render } from '@testing-library/react';
+import DestinosPrecios from './DestinosPrecios';
+
+// Prueba básica para verificar que la sección de destinos y precios se renderiza correctamente
+test('Renderiza la sección de destinos y precios correctamente', () => {
+    render(<DestinosPrecios />);
+}); 
