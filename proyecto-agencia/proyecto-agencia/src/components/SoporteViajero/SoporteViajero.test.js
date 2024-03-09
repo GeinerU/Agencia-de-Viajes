fbb19b24a8ca7ae0a623a265b16ca86cbc4edf29
geiner <uribegeiner03@gmail.com>
2024-03-09 02:48:16 -0500

    Se agrega codigo para prueba de Soporte al Viajero en archivo en SoporteViajero.test.js

--- proyecto-agencia/proyecto-agencia/src/components/SoporteViajero/SoporteViajero.test.js
+++ proyecto-agencia/proyecto-agencia/src/components/SoporteViajero/SoporteViajero.test.js
@@ -1,1 +1,9 @@
- 
+// Importa las bibliotecas necesarias para las pruebas
+import React from 'react';
+import { render } from '@testing-library/react';
+import SoporteViajero from './SoporteViajero';
+
+// Prueba básica para verificar que la sección de soporte al viajero se renderiza correctamente
+test('Renderiza la sección de soporte al viajero correctamente', () => {
+    render(<SoporteViajero />);
+}); 
