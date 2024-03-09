925868a85d1036afd0bdf6351f1c488134acf651
geiner <uribegeiner03@gmail.com>
2024-03-09 02:08:38 -0500

    Se agrega codigo correspondiente a CreacionPerfil.js

--- proyecto-agencia/proyecto-agencia/src/components/CreacionPerfil/CreacionPerfil.js
+++ proyecto-agencia/proyecto-agencia/src/components/CreacionPerfil/CreacionPerfil.js
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+import './CreacionPerfil.css';
+
+function CreacionPerfil() {
+    return (
+        <section id="perfil">
+            {/* Contenido de la sección Creación de Perfil */}
+        </section>
+    );
+}
+
+export default CreacionPerfil;
