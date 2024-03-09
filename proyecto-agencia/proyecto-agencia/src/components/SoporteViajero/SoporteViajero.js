029b34775abe3ccdedc4acea2344b3eae80f6f79
geiner <uribegeiner03@gmail.com>
2024-03-09 02:19:34 -0500

    Se agrega codigo correspondiente a SoporteViajero.js

--- proyecto-agencia/proyecto-agencia/src/components/SoporteViajero/SoporteViajero.js
+++ proyecto-agencia/proyecto-agencia/src/components/SoporteViajero/SoporteViajero.js
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+import './SoporteViajero.css';
+
+function SoporteViajero() {
+    return (
+        <section id="soporte">
+            {/* Contenido de la sección Soporte al Viajero */}
+        </section>
+    );
+}
+
+export default SoporteViajero;
