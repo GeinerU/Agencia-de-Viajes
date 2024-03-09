24c6dbb5daf3eb73c3f86db8bc5fefb99863cec7
geiner <uribegeiner03@gmail.com>
2024-03-09 02:16:43 -0500

    Se agrega codigo correspondiente a PlanesOfrecidos.js

--- proyecto-agencia/proyecto-agencia/src/components/PlanesOfrecidos/PlanesOfrecidos.js
+++ proyecto-agencia/proyecto-agencia/src/components/PlanesOfrecidos/PlanesOfrecidos.js
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+import './PlanesOfrecidos.css';
+
+function PlanesOfrecidos() {
+    return (
+        <section id="planes">
+            {/* Contenido de la sección Planes Ofrecidos */}
+        </section>
+    );
+}
+
+export default PlanesOfrecidos;
