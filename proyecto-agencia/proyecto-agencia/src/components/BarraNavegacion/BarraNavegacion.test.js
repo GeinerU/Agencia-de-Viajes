c1c899cec7f38a1a7ddcbd41dc347471eaa6e8bf
geiner <uribegeiner03@gmail.com>
2024-03-09 02:42:40 -0500

    Se agrega codigo para prueba de Barra de Navegación en archivo en BarraNavegación.test.js

--- proyecto-agencia/proyecto-agencia/src/components/BarraNavegacion/BarraNavegacion.test.js
+++ proyecto-agencia/proyecto-agencia/src/components/BarraNavegacion/BarraNavegacion.test.js
@@ -1,1 +1,9 @@
- 
+// Importa las bibliotecas necesarias para las pruebas
+import React from 'react';
+import { render } from '@testing-library/react';
+import BarraNavegacion from './BarraNavegacion';
+
+// Prueba básica para verificar que la barra de navegación se renderiza correctamente
+test('Renderiza la barra de navegación correctamente', () => {
+    render(<BarraNavegacion />);
+}); 
