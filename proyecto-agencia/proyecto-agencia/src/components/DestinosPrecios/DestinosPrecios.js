34a67822136885cdfbbb31ab21881072d0a35c4f
geiner <uribegeiner03@gmail.com>
2024-03-09 02:09:40 -0500

    Se agrega codigo correspondiente a BarraNavegación.js

--- proyecto-agencia/proyecto-agencia/src/components/DestinosPrecios/DestinosPrecios.js
+++ proyecto-agencia/proyecto-agencia/src/components/DestinosPrecios/DestinosPrecios.js
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+import './DestinosPrecios.css';
+
+function DestinosPrecios() {
+    return (
+        <section id="destinos">
+            {/* Contenido de la sección Destinos y Precios */}
+        </section>
+    );
+}
+
+export default DestinosPrecios;
