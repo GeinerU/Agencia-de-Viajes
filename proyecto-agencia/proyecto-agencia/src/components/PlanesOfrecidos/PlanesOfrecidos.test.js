4374d06dec8c147115267cd61d557e064977e96c
geiner <uribegeiner03@gmail.com>
2024-03-09 02:46:20 -0500

    Se agrega codigo para prueba de Planes Ofrecidos en archivo en PlanesOfrecidos.test.js

--- proyecto-agencia/proyecto-agencia/src/components/PlanesOfrecidos/PlanesOfrecidos.test.js
+++ proyecto-agencia/proyecto-agencia/src/components/PlanesOfrecidos/PlanesOfrecidos.test.js
@@ -1,1 +1,9 @@
- 
+// Importa las bibliotecas necesarias para las pruebas
+import React from 'react';
+import { render } from '@testing-library/react';
+import PlanesOfrecidos from './PlanesOfrecidos';
+
+// Prueba básica para verificar que la lista de planes ofrecidos se renderiza correctamente
+test('Renderiza la lista de planes ofrecidos correctamente', () => {
+    render(<PlanesOfrecidos />);
+}); 
