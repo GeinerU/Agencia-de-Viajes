7c1d74a0ee7b4b6aae01e27c6d5339297361456f
geiner <uribegeiner03@gmail.com>
2024-03-09 02:47:13 -0500

    Se agrega codigo para prueba de Quienes Somos en archivo en QuienesSomos.test.js

--- proyecto-agencia/proyecto-agencia/src/components/QuienesSomos/QuienesSomos.test.js
+++ proyecto-agencia/proyecto-agencia/src/components/QuienesSomos/QuienesSomos.test.js
@@ -1,1 +1,9 @@
- 
+// Importa las bibliotecas necesarias para las pruebas
+import React from 'react';
+import { render } from '@testing-library/react';
+import QuienesSomos from './QuienesSomos';
+
+// Prueba básica para verificar que la sección "Quienes Somos" se renderiza correctamente
+test('Renderiza la sección "Quienes Somos" correctamente', () => {
+    render(<QuienesSomos />);
+}); 
