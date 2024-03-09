7ab21279603b5be46da2cbc872de61dfa8dc7d5b
geiner <uribegeiner03@gmail.com>
2024-03-09 02:17:42 -0500

    Se agrega codigo correspondiente a PQR.js

--- proyecto-agencia/proyecto-agencia/src/components/PQR/PQR.js
+++ proyecto-agencia/proyecto-agencia/src/components/PQR/PQR.js
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+import './PQR.css';
+
+function PQR() {
+    return (
+        <section id="quejasReclamos">
+            {/* Contenido de la sección PQR */}
+        </section>
+    );
+}
+
+export default PQR;
